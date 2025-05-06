8bc2ff27a99f0287807d1e10505613fc35102628
SkyGodLucky <sebastien.pollion@outlook.fr>
2025-05-06 11:51:41 +0200

    modification routers

--- router/ingredientRouter.js
+++ router/ingredientRouter.js
@@ -5,7 +5,7 @@ const ingredientController = require('../controllers/ingredientController');
 // Routes pour les ingrédients
 ingredientRouter.post('/recipe/:recipeId/ingredient', ingredientController.createIngredient);
 ingredientRouter.get('/recipe/:recipeId/ingredients', ingredientController.getIngredientsByRecipeId);
-ingredientRouter.put('/:id', ingredientController.updateIngredient);
-ingredientRouter.delete('/:id', ingredientController.deleteIngredient);
+ingredientRouter.put('recipe/:id', ingredientController.updateIngredient);
+ingredientRouter.delete('recipe/:id', ingredientController.deleteIngredient);
 
 module.exports = ingredientRouter; 
--- router/recipeRouter.js
+++ router/recipeRouter.js
@@ -3,11 +3,11 @@ const recipeModel = require('../models/recipeModel');
 const recipeController = require('../controllers/recipeController');
 
 // Routes pour les recettes
-recipeRouter.post('/', recipeController.createRecipe);
-recipeRouter.get('/', recipeController.getAllRecipes);
-recipeRouter.get('/search', recipeController.searchRecipes);
-recipeRouter.get('/:id', recipeController.getRecipeById);
-recipeRouter.put('/:id', recipeController.updateRecipe);
-recipeRouter.delete('/:id', recipeController.deleteRecipe);
+recipeRouter.post('/recipe', recipeController.createRecipe);
+recipeRouter.get('/recipe', recipeController.getAllRecipes);
+recipeRouter.get('/recipe/search', recipeController.searchRecipes);
+recipeRouter.get('/recipe/:id', recipeController.getRecipeById);
+recipeRouter.put('/recipe/:id', recipeController.updateRecipe);
+recipeRouter.delete('/recipe/:id', recipeController.deleteRecipe);
 
 module.exports = recipeRouter; 
